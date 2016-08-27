f26c2720a83bea09f80833c187aafee3b82cadde
idk <idk@devuan.local>
2016-08-27 00:43:38 -0400

    created some functions to call debootstrap more effectively and allow myself to build a cache

--- hooks/preferences.d.sh
+++ hooks/preferences.d.sh
@@ -14,7 +14,7 @@ Pin-Priority: 200
 Package: *
 Pin: release a=nightly
 Pin-Priority: 100
-" | sudo tee $PRFDIR/be-stable
+" | sudo tee $IMGMAKER_APT_PREF_DIR/be-stable
 
 echo "Package: systemd
 Pin: release o=Debian
@@ -25,4 +25,4 @@ Pin-Priority: -1
 Package: systemd-sysv
 Pin: release o=Debian
 Pin-Priority: -1
-" | sudo tee $PRFDIR/avoid-systemd
+" | sudo tee $IMGMAKER_APT_PREF_DIR/avoid-systemd
